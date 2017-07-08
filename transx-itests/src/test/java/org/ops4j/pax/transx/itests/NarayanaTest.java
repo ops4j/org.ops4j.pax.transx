361d360d9e272e03ab1987fedd97fcfb50bc40de
Guillaume Nodet <gnodet@apache.org>
2017-07-08 08:42:11 +0200

    Add minimal integration tests

---
+++ transx-itests/src/test/java/org/ops4j/pax/transx/itests/NarayanaTest.java
@@ -0,0 +1,92 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.transx.itests;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.util.Filter;
+import org.ops4j.pax.transx.connector.ConnectionManagerFactory;
+import org.ops4j.pax.transx.jdbc.ManagedConnectionFactoryFactory;
+import org.ops4j.pax.transx.tm.TransactionManager;
+import org.osgi.service.jdbc.DataSourceFactory;
+
+import javax.inject.Inject;
+import javax.resource.spi.ConnectionManager;
+import javax.resource.spi.ManagedConnectionFactory;
+import javax.sql.DataSource;
+import javax.sql.XADataSource;
+import java.util.Properties;
+
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.transx.itests.TestConfiguration.mvnBundle;
+import static org.ops4j.pax.transx.itests.TestConfiguration.regressionDefaults;
+
+@RunWith(PaxExam.class)
+public class NarayanaTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Inject
+    @Filter(value = "(osgi.jdbc.driver.name=H2 JDBC Driver)")
+    private DataSourceFactory dsf;
+
+    @Inject
+    private TransactionManager tm;
+
+    @Configuration
+    public Option[] config() throws Exception {
+        return options(
+                regressionDefaults(),
+                mvnBundle("org.apache.geronimo.specs", "geronimo-jta_1.1_spec"),
+                mvnBundle("org.apache.geronimo.specs", "geronimo-j2ee-connector_1.6_spec"),
+                mvnBundle("org.apache.geronimo.specs", "geronimo-jms_2.0_spec"),
+                mvnBundle("org.ops4j.pax.transx", "transx-tm-api"),
+                mvnBundle("org.ops4j.pax.transx", "transx-tm-narayana"),
+                mvnBundle("org.ops4j.pax.transx", "transx-connector"),
+                mvnBundle("org.ops4j.pax.transx", "transx-jms"),
+                mvnBundle("org.ops4j.pax.transx", "transx-jdbc"),
+                mvnBundle("com.h2database", "h2"),
+                systemProperty("com.arjuna.ats.arjuna.recovery.periodicRecoveryInitilizationOffset").value("1")
+        );
+    }
+
+    @Test
+    public void createJdbcResource() throws Exception {
+        System.out.println("DSF: " + dsf);
+
+        Properties jdbc = new Properties();
+        jdbc.setProperty("url", "jdbc:h2:mem:test");
+        jdbc.setProperty("user", "sa");
+        jdbc.setProperty("password", "");
+        XADataSource xaDs = dsf.createXADataSource(jdbc);
+        ManagedConnectionFactory mcf = ManagedConnectionFactoryFactory.create(xaDs);
+        ConnectionManager cm = ConnectionManagerFactory.builder()
+                .transaction(ConnectionManagerFactory.TransactionSupportLevel.Xa)
+                .managedConnectionFactory(mcf)
+                .transactionManager(tm)
+                .name("h2")
+                .build();
+        DataSource ds = (DataSource) mcf.createConnectionFactory(cm);
+
+        Thread.sleep(5000);
+    }
+
+}
---
+++ transx-itests/src/test/java/org/ops4j/pax/transx/itests/TestConfiguration.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.transx.itests;
+
+import org.ops4j.pax.exam.CoreOptions;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.options.FrameworkPropertyOption;
+import org.ops4j.pax.exam.options.MavenArtifactProvisionOption;
+import org.ops4j.pax.exam.util.PathUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+
+import static org.ops4j.pax.exam.CoreOptions.*;
+
+public class TestConfiguration {
+
+    public static Option regressionDefaults() {
+        Properties props = new Properties();
+        try (InputStream is = TestConfiguration.class.getResourceAsStream("/osgi.properties")) {
+            props.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<FrameworkPropertyOption> frameworkOptions = new ArrayList<>();
+        for (Enumeration<?> e = props.propertyNames(); e.hasMoreElements();) {
+            String k = (String) e.nextElement();
+            frameworkOptions.add(CoreOptions.frameworkProperty(k).value(props.getProperty(k)));
+        }
+        return composite(
+                // Framework
+                frameworkProperties(frameworkOptions.toArray(new FrameworkPropertyOption[frameworkOptions.size()])),
+                // Logging
+                mvnBundle("org.slf4j", "slf4j-api"),
+                mvnBundle("ch.qos.logback", "logback-core"),
+                mvnBundle("ch.qos.logback", "logback-classic"),
+                mvnBundle("org.jboss.logging", "jboss-logging"),
+                // Set logback configuration via system property.
+                // This way, both the driver and the container use the same configuration
+                systemProperty("logback.configurationFile").value(
+                        "file:" + PathUtils.getBaseDir() + "/src/test/resources/logback.xml"),
+                // JUnit
+                junitBundles(),
+                // Config Admin
+                mvnBundle("org.apache.felix", "org.apache.felix.configadmin")
+        );
+    }
+
+    public static MavenArtifactProvisionOption mvnBundle(String groupId, String artifactId) {
+        return mavenBundle(groupId, artifactId).versionAsInProject();
+    }
+
+}
