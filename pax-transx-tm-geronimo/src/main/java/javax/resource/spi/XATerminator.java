4452dc393138e969f32ee6bf77d2f78f7412e735
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-04-12 16:55:24 +0200

    [build] Implement and enforce license headers

--- pax-transx-tm-geronimo/src/main/java/javax/resource/spi/XATerminator.java
+++ pax-transx-tm-geronimo/src/main/java/javax/resource/spi/XATerminator.java
@@ -7,7 +7,7 @@
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -16,13 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
-//
-// This source code implements specifications defined by the Java
-// Community Process. In order to remain compliant with the specification
-// DO NOT add / change / or delete method signatures!
-//
-
 package javax.resource.spi;
 
 import javax.transaction.xa.XAException;
