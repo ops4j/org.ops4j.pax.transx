5306c8b3155925cca0edbed32d407f5d1609f1d3
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-02-02 13:19:00 +0100

    #34: Set com.atomikos.logging.Slf4JLoggerFactoryDelegate as default

--- pax-transx-tm-atomikos/src/main/java/com/atomikos/logging/LoggerFactory.java
+++ pax-transx-tm-atomikos/src/main/java/com/atomikos/logging/LoggerFactory.java
@@ -14,7 +14,7 @@ public final class LoggerFactory {
 
 	}
 
-	static LoggerFactoryDelegate loggerFactoryDelegate;
+	static LoggerFactoryDelegate loggerFactoryDelegate = new Slf4JLoggerFactoryDelegate();
 
 	public static final Logger createLogger(Class<?> clazz) {
 		return loggerFactoryDelegate.createLogger(clazz);
@@ -24,54 +24,4 @@ public final class LoggerFactory {
 		LoggerFactory.loggerFactoryDelegate = loggerFactoryDelegate;
 	}
 
-	static {
-		String cname = null;
-		//let's try with
-		try {
-			Class.forName("org.slf4j.impl.StaticLoggerBinder");
-			cname = "com.atomikos.logging.Slf4JLoggerFactoryDelegate";
-		} catch (Throwable ex) {
-			System.out.println("No org.slf4j.impl.StaticLoggerBinder found in ClassPath, trying with log4j2...");
-		}
-		
-		if(cname==null){
-			try {
-				Class.forName("org.apache.logging.log4j.Logger");
-				cname = "com.atomikos.logging.Log4j2LoggerFactoryDelegate";
-			} catch (Throwable ex) {
-				//don't print stackTrace - cf bug 118228
-				System.out.println("No org.apache.logging.log4j.Logger found found in ClassPath, trying with log4j...");
-			}
-		}
-
-		if(cname==null){
-			try {
-				Class.forName("org.apache.log4j.Logger");
-				cname = "com.atomikos.logging.Log4JLoggerFactoryDelegate";
-			} catch (Throwable ex) {
-				System.out.println("No org.apache.log4j.Logger found found in ClassPath, falling back default...");
-			}
-		}
-
-		try {
-			if (cname != null) {
-				Class<?> loggerClass = (Class<?>) Class.forName(cname.trim(), true, LoggerFactory.class.getClassLoader());
-				loggerFactoryDelegate = (LoggerFactoryDelegate) loggerClass.newInstance();
-			} else {
-				fallbackToDefault();
-			}
-		} catch (Throwable ex) {
-			// ignore - if we get here, some issue prevented the logger class
-			// from being loaded.
-			// maybe a ClassNotFound or NoClassDefFound or similar. Just use
-			// j.u.l
-			fallbackToDefault();
-		}
-		Logger logger = createLogger(LoggerFactory.class);
-		logger.logDebug("Using " + loggerFactoryDelegate + " for logging.");
-	}
-
-	private static void fallbackToDefault() {
-		setLoggerFactoryDelegate(new JULLoggerFactoryDelegate());
-	}
 }
