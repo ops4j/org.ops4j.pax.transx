edb5460db8e802541c039d74ea0e57b41b85ee53
Grzegorz Grzybek <gr.grzybek@gmail.com>
2021-02-12 15:28:51 +0100

    Fix checkstyle issues

--- pax-transx-tm-atomikos/src/main/java/com/atomikos/logging/LoggerFactory.java
+++ pax-transx-tm-atomikos/src/main/java/com/atomikos/logging/LoggerFactory.java
@@ -17,48 +17,47 @@ package com.atomikos.logging;
 
 public final class LoggerFactory {
 
-	private LoggerFactory() {
+    static LoggerFactoryDelegate loggerFactoryDelegate;
 
-	}
+    private LoggerFactory() {
+    }
 
-	static LoggerFactoryDelegate loggerFactoryDelegate;
+    public static Logger createLogger(Class<?> clazz) {
+        return loggerFactoryDelegate.createLogger(clazz);
+    }
 
-	public static Logger createLogger(Class<?> clazz) {
-		return loggerFactoryDelegate.createLogger(clazz);
-	}
+    static void setLoggerFactoryDelegate(LoggerFactoryDelegate loggerFactoryDelegate) {
+        LoggerFactory.loggerFactoryDelegate = loggerFactoryDelegate;
+    }
 
-	static void setLoggerFactoryDelegate(LoggerFactoryDelegate loggerFactoryDelegate) {
-		LoggerFactory.loggerFactoryDelegate = loggerFactoryDelegate;
-	}
+    static {
+        String cname = null;
+        //let's try with
+        try {
+            Class.forName("org.slf4j.LoggerFactory");
+            cname = "com.atomikos.logging.Slf4JLoggerFactoryDelegate";
+        } catch (Throwable ignored) {
+        }
 
-	static {
-		String cname = null;
-		//let's try with
-		try {
-			Class.forName("org.slf4j.LoggerFactory");
-			cname = "com.atomikos.logging.Slf4JLoggerFactoryDelegate";
-		} catch (Throwable ignored) {
-		}
+        try {
+            if (cname != null) {
+                Class<?> loggerClass = Class.forName(cname.trim(), true, LoggerFactory.class.getClassLoader());
+                loggerFactoryDelegate = (LoggerFactoryDelegate) loggerClass.newInstance();
+            } else {
+                fallbackToDefault();
+            }
+        } catch (Throwable ex) {
+            // ignore - if we get here, some issue prevented the logger class
+            // from being loaded.
+            // maybe a ClassNotFound or NoClassDefFound or similar. Just use
+            // j.u.l
+            fallbackToDefault();
+        }
+        Logger logger = createLogger(LoggerFactory.class);
+        logger.logDebug("Using " + loggerFactoryDelegate + " for logging.");
+    }
 
-		try {
-			if (cname != null) {
-				Class<?> loggerClass = Class.forName(cname.trim(), true, LoggerFactory.class.getClassLoader());
-				loggerFactoryDelegate = (LoggerFactoryDelegate) loggerClass.newInstance();
-			} else {
-				fallbackToDefault();
-			}
-		} catch (Throwable ex) {
-			// ignore - if we get here, some issue prevented the logger class
-			// from being loaded.
-			// maybe a ClassNotFound or NoClassDefFound or similar. Just use
-			// j.u.l
-			fallbackToDefault();
-		}
-		Logger logger = createLogger(LoggerFactory.class);
-		logger.logDebug("Using " + loggerFactoryDelegate + " for logging.");
-	}
-
-	private static void fallbackToDefault() {
-		setLoggerFactoryDelegate(new JULLoggerFactoryDelegate());
-	}
+    private static void fallbackToDefault() {
+        setLoggerFactoryDelegate(new JULLoggerFactoryDelegate());
+    }
 }
